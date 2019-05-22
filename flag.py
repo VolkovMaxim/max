65910c28fe811159873275592ca5a5d3d278ee79
VolkovMaxim <50923045+VolkovMaxim@users.noreply.github.com>
2019-05-22 20:48:09 +0300

    Флаги
    Напишите программу, которая по данному числу n от 1 до 9 выводит на экран n флагов. Изображение одного флага имеет размер 4×4 символов, между двумя соседними флагами также имеется пустой (из пробелов) столбец. Разрешается вывести пустой столбец после последнего флага. Внутри каждого флага должен быть записан его номер — число от 1 до n.

---
+++ flag.py
@@ -0,0 +1,13 @@
+q = int(input())
+
+for i in range(q):
+    print('+___', end=' ')
+print()
+for i in range(q):
+    print('|', i + 1, ' /', sep='', end=' ')
+print()
+for i in range(q):
+    print('|__\ ', sep='', end='')
+print()
+for i in range(q):
+    print('|    ', sep='', end='')
